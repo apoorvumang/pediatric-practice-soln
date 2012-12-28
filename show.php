02349dc53af28a6f1a06f4a0e2ab167a105152e4
Apoorv Umang <apoorvumang@gmail.com>
2012-12-28 21:39:46 +0530

    removed list from show.php

--- show.php
+++ show.php
@@ -46,33 +46,4 @@ $(function() {
 	<input type="text" name="dob_show" id="dob_show" />
 	<input type="submit" name="submit" value="Go" />
 </form>
-<table>
-	<tbody>
-		<tr>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Date of Birth</th>
-			<th>Phone</th>
-			<th>Sex</th>
-		</tr>
-		<?php $result = mysqli_query($link, "SELECT * FROM patients WHERE 1 ORDER BY id");
-		while($row = mysqli_fetch_assoc($result))
-		{
-			
-			echo "<tr>";
-			echo "<td>".$row['id']."</td>";
-			echo "<td><a href=edit-sched.php?id=".$row['id'].">";
-			echo $row['name'];
-			echo "</a></td>";
-			echo "<td>".$row['dob']."</td>";
-			echo "<td>".$row['phone']."</td>";
-			echo "<td>".$row['sex']."</td>";
-			echo "</tr>";
-			
-		}
-		?>
-	</tbody>
-</table>
-
-
 <?php include('footer.php'); ?>
