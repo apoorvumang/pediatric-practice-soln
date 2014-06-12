bed32906c4314e00229f24a61ec189f064a35ec5
Apoorv Umang <apoorvumang@gmail.com>
2014-06-13 03:41:57 +0530

    added scripts for deploying

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh drmahima.com@ssh.drmahima.com 'bash -s' < git-pull.sh
---
+++ git-pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /www/test/mummy-software
+git pull
+# 'echo $PWD',
+# 		'whoami',
+# 		'git pull',
+# 		'git status',
+# 		'git submodule sync',
+# 		'git submodule update',
+# 		'git submodule status',
