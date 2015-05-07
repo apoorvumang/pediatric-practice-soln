969bd38b310758a0467181c1058c8eca2b278e56
Apoorv Umang Saxena <apoorv.saxena@flipkart.com>
2015-05-07 17:03:25 +0530

    testing again..

--- email.php
+++ email.php
@@ -40,50 +40,35 @@ $subject = 'Test email with attachment';
 //so we use the MD5 algorithm to generate a random hash
 $random_hash = md5(date('r', time()));
 //define the headers we want passed. Note that they are separated with \r\n
-$headers .= "From: Dr. Mahima <mahima@drmahima.com>\r\n";
 //add boundary string and mime type specification
-$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
 //read the atachment file contents into a string,
 //encode it with MIME base64,
 //and split it into smaller chunks
 $attachment = chunk_split(base64_encode(file_get_contents("http://www.drmahima.com/patient/pdf.php?id=69")));
 //define the body of the message.
-ob_start(); //Turn on output buffering
-?>
---PHP-mixed-<?php echo $random_hash; ?>
-Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>"
+//
+$message = "Hello World";
+    $name = "shit.pdf";
+    $header = "From: "."Dr. Mahima <mahima@drmahima.com>"."\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: multipart/mixed; boundary=\"".$random_hash."\"\r\n\r\n";
+    $header .= "This is a multi-part message in MIME format.\r\n";
+    $header .= "--".$random_hash."\r\n";
+    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    $header .= $message."\r\n\r\n";
+    $header .= "--".$random_hash."\r\n";
+    $header .= "Content-Type: application/octet-stream; name=\"".$name."\"\r\n"; // use different content types here
+    $header .= "Content-Transfer-Encoding: base64\r\n";
+    $header .= "Content-Disposition: attachment; filename=\"".$name."\"\r\n\r\n";
+    $header .= $atachment."\r\n\r\n";
+    $header .= "--".$random_hash."--";
+    if (mail($to, $subject, "", $header)) {
+        echo "mail send ... OK"; // or use booleans here
+    } else {
+        echo "mail send ... ERROR!";
+    }
 
---PHP-alt-<?php echo $random_hash; ?>
-Content-Type: text/plain; charset="iso-8859-1"
-Content-Transfer-Encoding: 7bit
-
-Hello World!!!
-This is simple text email message.
-
---PHP-alt-<?php echo $random_hash; ?>
-Content-Type: text/html; charset="iso-8859-1"
-Content-Transfer-Encoding: 7bit
-
-<h2>Hello World!</h2>
-<p>This is something with <b>HTML</b> formatting.</p>
-
---PHP-alt-<?php echo $random_hash; ?>--
-
---PHP-mixed-<?php echo $random_hash; ?>
-Content-Type: application/zip; name="attachment.zip"
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment
-
-<?php echo $attachment; ?>
---PHP-mixed-<?php echo $random_hash; ?>--
-
-<?php
-//copy current buffer contents into $message variable and delete current output buffer
-$message = ob_get_clean();
-//send the email
-$mail_sent = @mail( $to, $subject, $message, $headers );
-//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
-echo $mail_sent ? "Mail sent" : "Mail failed";
 ?>
 Sent mail!
 Mail content: <br><br>
