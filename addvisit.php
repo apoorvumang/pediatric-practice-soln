031a2067cd0f9ea42015d6dc9a3bb0c18f1f3af9
Apoorv Umang <apoorvumang@gmail.com>
2016-10-11 17:18:01 +0530

    x

--- addvisit.php
+++ addvisit.php
@@ -19,23 +19,52 @@ $(function() {
     dateFormat:"Y-m-d"
   });
 });
+
+function autocomplet() {
+  var min_length = 3; // min characters to display the autocomplete
+  var keyword = $('#patient_id').val();
+  var myId = 0;
+  if (keyword.length >= min_length) {
+    $.ajax({
+      url: 'ajax_refresh.php',
+      type: 'POST',
+      data: {"keyword":keyword, "myid": myId},
+      success:function(data){
+        $('#patient_autocomplet_list').show();
+        $('#patient_autocomplet_list').html(data);
+      }
+    });
+  } else {
+    $('#patient_autocomplet_list').hide();
+  }
+}
+
+function set_item(item) {
+  // change input value
+  $('#patient_id').val(item);
+  // hide proposition list
+  $('#patient_autocomplet_list').hide();
+}
+
 </script>
 <h3>Add Visit</h3>
 <form action="" method="post" enctype="multipart/form-data" style="width:auto" name="1">
   <label for="date">Date : &nbsp;&nbsp;&nbsp;&nbsp;</label>
   <input type="text" name="date" id="date" style="margin-right:40px;" value= <?php echo "\"".date("Y-m-d")."\""; ?>/>
 
-
+  <br>
   <label for="p_id">Patient ID:&nbsp;&nbsp;</label>
   <input type="text" id = "patient_id" name ="p_id" onkeyup="autocomplet()" />
   <ul id="patient_autocomplet_list"></ul>
 
-
+  <br>
   <label for="note">Additional info (height/weight): </label>
+  <br>
   <textarea name="note" rows="3" cols="50">
   Height:
   Weight: 
   </textarea>
+  <br>
 
 <input type="submit" name="addvisit" value="Go" />
 </form>
