af666c8221f82c2bd7be893457001b70fa70a1e4
Apoorv Umang <apoorvumang@gmail.com>
2016-10-16 10:26:08 +0530

    added dob in patient ajax list

--- ajax_refresh.php
+++ ajax_refresh.php
@@ -1,13 +1,13 @@
 <?php
 require('connect.php');
 include('header_db_link.php');
-$result =  mysqli_query($link, "SELECT name, id FROM patients WHERE name LIKE \"%".$_POST["keyword"]."%\" AND id NOT IN
+$result =  mysqli_query($link, "SELECT name, id, dob FROM patients WHERE name LIKE \"%".$_POST["keyword"]."%\" AND id NOT IN
 	(SELECT s_id FROM siblings WHERE p_id = {$_POST['myid']})");
 //autocomplete but dont choose self or those who are already siblings
 while($row = mysqli_fetch_assoc($result))
 {
 	if($_POST['myid'] != $row['id'])
-		echo "<li onclick=\"set_item('".$row['id']."');\">".$row['id']." ".$row['name']."</li>";
+		echo "<li onclick=\"set_item('".$row['id']."');\">".$row['id']." ".$row['name']." ".date('M Y', strtotime($row['dob']))."</li>";
 }
 // echo $var;
 // echo "SELECT name, id FROM patients WHERE name LIKE \"%".$_POST["keyword"]."%\"";
