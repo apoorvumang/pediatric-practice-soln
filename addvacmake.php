021a903f97b71e2a7d272f069c5a47a3962ac998
Apoorv Umang <apoorvumang@gmail.com>
2017-08-11 17:13:54 +0530

    added functionality to add/edit products as well as their vaccine connections

--- addvacmake.php
+++ addvacmake.php
@@ -7,8 +7,12 @@ if(isset($_POST['submit']))
 	}
 	else
 	{
-		$_POST['name'] = mysqli_real_escape_string($link, $_POST['name']);
-		if(mysqli_query($link, "INSERT INTO vac_make(name) VALUES('{$_POST['name']}')"))
+		$description = $_POST['description'];
+		$name = mysqli_real_escape_string($link, $_POST['name']);
+		$price = $_POST['price'];
+		$for_invoice = $_POST['for_invoice'];
+		$query = "INSERT INTO vac_make(name, price, description, for_invoice) VALUES('{$name}', {$price}, '{$description}', '{$for_invoice}');";
+		if(mysqli_query($link, $query))
 		{
 			echo "Successfully added vaccine make {$_POST['name']}!";
 		}
@@ -21,6 +25,14 @@ if(isset($_POST['submit']))
 }
 ?>
 
+<style>
+input[type="checkbox"]{
+width: 30px;
+height: 30px;
+border-width: 0;
+}
+</style>
+
 
 <form action="" method="post" enctype="multipart/form-data">
 	<h3>Add Product</h3>
@@ -29,6 +41,41 @@ if(isset($_POST['submit']))
 	<label for="name">Name of product:</label><br />
 	<input type="text" name="name" id="name" autofocus="autofocus" />
 	</p>
+	<p>
+	<label for="price">Price:</label><br />
+	<input type="number" name="price" id="price"/>
+	</p>
+	<p>
+	<label for="description">Description:</label><br />
+	<input type="text" name="description" id="description"/>
+	</p>
+
+	<p>
+		<label for="for_invoice">Add to invoice list?</label>
+		<select name="for_invoice">
+		  <option value="Y">Y</option>
+		  <option value="N">N</option>
+		</select>
+	</p>
+
+	<h4>Select vaccines to associate with this product</h4>
+	<table>
+	<?php
+		$query = "SELECT id, name from vaccines WHERE 1";
+		$result = mysqli_query($link, $query);
+		while($vaccine = mysqli_fetch_assoc($result)) {
+			echo "<tr>";
+			echo "<td>";
+			echo "<input type='checkbox' name='associatedVaccine[]' value='{$vaccine['id']}'>";
+			echo "</td>";
+			echo "<td>";
+			echo "{$vaccine['name']}";
+			echo "</td>";
+			echo "</tr>";
+		}
+
+	?>
+</table>
 
 	<p>
 	<input type="submit" name="submit" value="Add"/>
--- changevacmake.php
+++ changevacmake.php
@@ -1,12 +1,29 @@
 <?php include('header.php');
 if($_POST['makechange']=='1')
 {
-	if(!$_POST['name'])
-		echo "Please enter a name!";
-	else if(mysqli_query($link, "UPDATE vac_make SET name='{$_POST['name']}' WHERE id={$_POST['id']}"))
-		echo "Changes save successfully!";
+	$name = $_POST['name'];
+	$id = $_POST['id']; // product id
+	$price = $_POST['price'];
+	$description = $_POST['description'];
+	$for_invoice = $_POST['for_invoice'];
+	$query = "UPDATE vac_make SET name='{$name}', price = {$price}, description='{$description}', for_invoice='{$for_invoice}' WHERE id={$_POST['id']}";
+	if(mysqli_query($link, $query))
+		echo "Edited vac_make successfully!";
 	else
-		echo "Error making changes.";
+		echo "Error making changes in vac_make :(";
+	$query = "DELETE from vac_to_make WHERE vm_id = {$id};"; // deleting old connections because we will be adding them all later
+	if(mysqli_query($link, $query)) {
+		echo "Deleted from vac_to_make successfully!";
+	} else {
+		echo "Error deleting from vac_to_make :(";
+	}
+	$vaccine_id_array = $_POST['associatedVaccine'];
+	foreach ($vaccine_id_array as $key => $vaccine_id) {
+		$query = "INSERT into vac_to_make(v_id, vm_id) VALUES ('{$vaccine_id}', '{$id}')";
+		if(!mysqli_query($link, $query)) {
+			echo "Some error adding vac_to_make row :(";
+		}
+	}
 }
 else if($_POST['choice']=="delete")
 {
@@ -23,18 +40,66 @@ else if($_POST['choice']=="delete")
 else if($_POST['choice']=="edit")
 {
 	$vac_make_row = mysqli_fetch_assoc(mysqli_query($link, "SELECT * FROM vac_make WHERE id={$_POST['id']}"));
+	$id = $_POST['id'];
+	$price = $vac_make_row['price'];
+	$description = $vac_make_row['description'];
+	$for_invoice = $vac_make_row['for_invoice'];
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
 	<h3>Edit Product</h3>
-	
+
+
 	<p>
 	<label for="name">Product:</label><br />
 	<input type="text" name="name" id="name" value=<?php echo "\"{$vac_make_row['name']}\"";?>/>
 	<input type="hidden" name="id" value=<?php echo "\"{$vac_make_row['id']}\"";?>/>
 	<input type="hidden" name="makechange" value="1"/>
 	</p>
+	<p>
+	<label for="price">Price:</label><br />
+	<input type="number" name="price" id="price" value=<?php echo "'{$price}'"; ?>/>
+	</p>
+	<p>
+	<label for="description">Description:</label><br />
+	<input type="text" name="description" id="description" value=<?php echo "'{$description}'"; ?>/>
+	</p>
 
+	<p>
+		<label for="for_invoice">Add to invoice list?</label>
+		<select name="for_invoice">
+		  <option value="Y" <?php if($for_invoice == 'Y') {echo 'selected';} ?>>Y</option>
+		  <option value="N" <?php if($for_invoice == 'N') {echo 'selected';} ?>>N</option>
+		</select>
+	</p>
+	<h4>Select vaccines to associate with this product</h4>
+	<table>
+	<?php
+		$query = "SELECT id, name from vaccines WHERE 1";
+		$result = mysqli_query($link, $query);
+		$query = "SELECT * FROM vac_to_make WHERE vm_id = {$id}";
+		$result2 = mysqli_query($link, $query);
+		$v_id_array = [];
+		while($vaccine_associated_with_vm = mysqli_fetch_assoc($result2)) {
+			$v_id_array[] = $vaccine_associated_with_vm['v_id'];
+		}
+		while($vaccine = mysqli_fetch_assoc($result)) {
+			echo "<tr>";
+			echo "<td>";
+			echo "<input type='checkbox' name='associatedVaccine[]' value='{$vaccine['id']}'";
+			if(in_array($vaccine['id'], $v_id_array)) {
+				echo " checked ";
+			}
+			echo ">";
+			echo "</td>";
+			echo "<td>";
+			echo "{$vaccine['name']}";
+			echo "</td>";
+			echo "</tr>";
+		}
+
+	?>
+	</table>
 	<p>
 	<input type="submit" name="submit" value="Save changes"/>
 	</p>
@@ -68,6 +133,6 @@ else {
 	</p>
 </form>
 
-<?php 
+<?php
 }
 include('footer.php'); ?>
