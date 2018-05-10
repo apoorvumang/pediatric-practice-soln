a0d84114d2deea5f961f5d1929a5aa849bef384e
Apoorv Umang <apoorvumang@gmail.com>
2018-05-10 13:23:41 +0530

    changed sms gateway

--- smsGateway.php
+++ smsGateway.php
@@ -1,113 +1,29 @@
 <?php
 
-    class SmsGateway {
-
-        static $baseUrl = "https://smsgateway.me";
-
-
-        function __construct($email,$password) {
-            $this->email = $email;
-            $this->password = $password;
-        }
-
-        function createContact ($name,$number) {
-            return $this->makeRequest('/api/v3/contacts/create','POST',['name' => $name, 'number' => $number]);
-        }
-
-        function getContacts ($page=1) {
-           return $this->makeRequest('/api/v3/contacts','GET',['page' => $page]);
-        }
-
-        function getContact ($id) {
-            return $this->makeRequest('/api/v3/contacts/view/'.$id,'GET');
-        }
-
-
-        function getDevices ($page=1)
-        {
-            return $this->makeRequest('/api/v3/devices','GET',['page' => $page]);
-        }
-
-        function getDevice ($id)
-        {
-            return $this->makeRequest('/api/v3/devices/view/'.$id,'GET');
-        }
-
-        function getMessages($page=1)
-        {
-            return $this->makeRequest('/api/v3/messages','GET',['page' => $page]);
-        }
-
-        function getMessage($id)
-        {
-            return $this->makeRequest('/api/v3/messages/view/'.$id,'GET');
-        }
-
-        function sendMessageToNumber($to, $message, $device, $options=[]) {
-            $query = array_merge(['number'=>$to, 'message'=>$message, 'device' => $device], $options);
-            return $this->makeRequest('/api/v3/messages/send','POST',$query);
-        }
-
-        function sendMessageToManyNumbers ($to, $message, $device, $options=[]) {
-            $query = array_merge(['number'=>$to, 'message'=>$message, 'device' => $device], $options);
-            return $this->makeRequest('/api/v3/messages/send','POST', $query);
-        }
-
-        function sendMessageToContact ($to, $message, $device, $options=[]) {
-            $query = array_merge(['contact'=>$to, 'message'=>$message, 'device' => $device], $options);
-            return $this->makeRequest('/api/v3/messages/send','POST', $query);
-        }
-
-        function sendMessageToManyContacts ($to, $message, $device, $options=[]) {
-            $query = array_merge(['contact'=>$to, 'message'=>$message, 'device' => $device], $options);
-            return $this->makeRequest('/api/v3/messages/send','POST', $query);
-        }
-
-        function sendManyMessages ($data) {
-            $query['data'] = $data;
-            return $this->makeRequest('/api/v3/messages/send','POST', $query);
-        }
-
-        private function makeRequest ($url, $method, $fields=[]) {
-
-            $fields['email'] = $this->email;
-            $fields['password'] = $this->password;
-
-            $url = smsGateway::$baseUrl.$url;
-
-            $fieldsString = http_build_query($fields);
-
-
-            $ch = curl_init();
-
-            if($method == 'POST')
-            {
-                curl_setopt($ch,CURLOPT_POST, count($fields));
-                curl_setopt($ch,CURLOPT_POSTFIELDS, $fieldsString);
-            }
-            else
-            {
-                $url .= '?'.$fieldsString;
-            }
-
-            curl_setopt($ch, CURLOPT_URL,$url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-            curl_setopt($ch, CURLOPT_HEADER , false);  // we want headers
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-            $result = curl_exec ($ch);
-
-            $return['response'] = json_decode($result,true);
-
-            if($return['response'] == false)
-                $return['response'] = $result;
-
-            $return['status'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-            curl_close ($ch);
-
-            return $return;
-        }
+class SmsGateway {
+    function sendMessageToNumber($to, $message) {
+      $message = addslashes($message);
+      $curl = curl_init();
+      curl_setopt_array($curl, array(
+       CURLOPT_URL => "http://fcm.googleapis.com/fcm/send",
+       CURLOPT_RETURNTRANSFER => true,
+       CURLOPT_ENCODING => "",
+       CURLOPT_MAXREDIRS => 10,
+       CURLOPT_TIMEOUT => 30,
+       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+       CURLOPT_CUSTOMREQUEST => "POST",
+       CURLOPT_POSTFIELDS => "\n{\n \"to\" : \"/topics/sms\",\n \"data\" : {\n\t\"phoneNumber\": \"{$to}\",\n\t\"message\": \"{$message}\"\n } \n}",
+       CURLOPT_HTTPHEADER => array(
+         "authorization: key=AAAAmiPEplA:APA91bHe_Lv8e0BUz39Jx-U_WnR1lOO3KftMSrGKsQRPBmMt4VmAahW0ISf-i4nJEjWLG0xqe4dhFXsgV-06fFBU4bAqgen62uFABzE5sxTbrVoyGoOeTTVno-auRCMyXLUyA6aatXKu",
+         "cache-control: no-cache",
+         "content-type: application/json"
+       ),
+      ));
+      $resp = curl_exec($curl);
+      $err = curl_error($curl);
+      curl_close($curl);
+      return $err;
     }
+}
 
 ?>
--- testsms.php
+++ testsms.php
@@ -1,15 +1,83 @@
 <?php
-include "smsGateway.php";
-$smsGateway = new SmsGateway('apoorvumang@gmail.com', 'vultr123');
+// include "smsGateway.php";
+// $smsGateway = new SmsGateway('apoorvumang@gmail.com', 'vultr123');
+//
+// $deviceID = 84213;
+// $number = '7891947877';
+// $message = 'Hello World!';
+//
+//
+// //Please note options is no required and can be left out
+// $result = $smsGateway->sendMessageToNumber($number, $message, $deviceID);
 
-$deviceID = 84213;
-$number = '7891947877';
-$message = 'Hello World!';
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+// curl_setopt_array($curl, array(
+//     CURLOPT_RETURNTRANSFER => 1,
+//     CURLOPT_URL => 'http://testcURL.com/?item1=value&item2=value2',
+//     CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+// ));
 
 
-//Please note options is no required and can be left out
-$result = $smsGateway->sendMessageToNumber($number, $message, $deviceID);
+
+curl_setopt_array($curl, array(
+ CURLOPT_URL => "http://fcm.googleapis.com/fcm/send",
+ CURLOPT_RETURNTRANSFER => true,
+ CURLOPT_ENCODING => "",
+ CURLOPT_MAXREDIRS => 10,
+ CURLOPT_TIMEOUT => 30,
+ CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+ CURLOPT_CUSTOMREQUEST => "POST",
+ CURLOPT_POSTFIELDS => "\n{\n \"to\" : \"/topics/sms\",\n \"data\" : {\n\t\"phoneNumber\": \"7407650530\",\n\t\"message\": \"test mesag2e\"\n } \n}",
+ CURLOPT_HTTPHEADER => array(
+   "authorization: key=AAAAmiPEplA:APA91bHe_Lv8e0BUz39Jx-U_WnR1lOO3KftMSrGKsQRPBmMt4VmAahW0ISf-i4nJEjWLG0xqe4dhFXsgV-06fFBU4bAqgen62uFABzE5sxTbrVoyGoOeTTVno-auRCMyXLUyA6aatXKu",
+   "cache-control: no-cache",
+   "content-type: application/json"
+ ),
+));
 
 
-var_dump($result);
+
+
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+$err = curl_error($curl);
+
+
+// Close request to clear up some resources
+curl_close($curl);
+
+var_dump($err);
+//
+// $curl = curl_init();
+//
+// curl_setopt_array($curl, array(
+//  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+//  CURLOPT_RETURNTRANSFER => true,
+//  CURLOPT_ENCODING => "",
+//  CURLOPT_MAXREDIRS => 10,
+//  CURLOPT_TIMEOUT => 30,
+//  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//  CURLOPT_CUSTOMREQUEST => "POST",
+//  CURLOPT_POSTFIELDS => "\n{\n \"to\" : \"/topics/sms\",\n \"data\" : {\n\t\"phoneNumber\": \"7407650530\",\n\t\"message\": \"test mesag2e\"\n } \n}",
+//  CURLOPT_HTTPHEADER => array(
+//    "authorization: key=AAAAmiPEplA:APA91bHe_Lv8e0BUz39Jx-U_WnR1lOO3KftMSrGKsQRPBmMt4VmAahW0ISf-i4nJEjWLG0xqe4dhFXsgV-06fFBU4bAqgen62uFABzE5sxTbrVoyGoOeTTVno-auRCMyXLUyA6aatXKu",
+//    "cache-control: no-cache",
+//    "content-type: application/json"
+//  ),
+// ));
+//
+// $response = curl_exec($curl);
+// $err = curl_error($curl);
+//
+// curl_close($curl);
+//
+// if ($err) {
+//  echo "cURL Error #:" . $err;
+// } else {
+//  echo $response;
+// }
+
+// var_dump($result);
 ?>
