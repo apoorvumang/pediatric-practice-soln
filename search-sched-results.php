97c7f7ce36aee5081ccf0f3157da04df4605ca34
Apoorv Saxena <apoorvumang@gmail.com>
2024-09-05 19:48:16 +0530

    switched to older logic but still added appt search to sched search

--- search-sched-results.php
+++ search-sched-results.php
@@ -15,45 +15,62 @@ $deviceID = 84200;
 
 if($_POST['specificdate']||$_POST['tofromdate']||$_POST['patientsearch'])	//If some submit button clicked
 {
-	if($_POST['specificdate'])
-	{
-		$_POST['date'] = date('Y-m-d', strtotime($_POST['date']));
-		$_POST['date'] = mysqli_real_escape_string($link, $_POST['date']);
-		$result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE date ='{$_POST['date']}' AND given='N'");
-		$nrows = mysqli_num_rows($result);
-	}
-	else if($_POST['tofromdate'])
-	{
-		$_POST['todate'] = date('Y-m-d', strtotime($_POST['todate']));
-		$_POST['todate'] = mysqli_real_escape_string($link, $_POST['todate']);
-		$_POST['fromdate'] = date('Y-m-d', strtotime($_POST['fromdate']));
-		$_POST['fromdate'] = mysqli_real_escape_string($link, $_POST['fromdate']);
+    $include_appointments = isset($_POST['include_appointments']);
 
-		$result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE date >='{$_POST['fromdate']}' AND date <='{$_POST['todate']}' AND given='N' ORDER BY date");
-		$nrows = mysqli_num_rows($result);
-	}
-	else if($_POST['patientsearch'])
-	{
-		$_POST['patientid'] = mysqli_real_escape_string($link, $_POST['patientid']);
-		$result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE p_id ='{$_POST['patientid']}' AND given='N' ORDER BY date");
-		$nrows = mysqli_num_rows($result);
-	}
+    if($_POST['specificdate'])
+    {
+        $_POST['date'] = date('Y-m-d', strtotime($_POST['date']));
+        $_POST['date'] = mysqli_real_escape_string($link, $_POST['date']);
+        $result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE date ='{$_POST['date']}' AND given='N'");
+        $nrows = mysqli_num_rows($result);
+
+        if ($include_appointments) {
+            $appt_result = mysqli_query($link, "SELECT * FROM appointments WHERE date ='{$_POST['date']}'");
+            $nrows += mysqli_num_rows($appt_result);
+        }
+    }
+    else if($_POST['tofromdate'])
+    {
+        $_POST['todate'] = date('Y-m-d', strtotime($_POST['todate']));
+        $_POST['todate'] = mysqli_real_escape_string($link, $_POST['todate']);
+        $_POST['fromdate'] = date('Y-m-d', strtotime($_POST['fromdate']));
+        $_POST['fromdate'] = mysqli_real_escape_string($link, $_POST['fromdate']);
+
+        $result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE date >='{$_POST['fromdate']}' AND date <='{$_POST['todate']}' AND given='N' ORDER BY date");
+        $nrows = mysqli_num_rows($result);
+
+        if ($include_appointments) {
+            $appt_result = mysqli_query($link, "SELECT * FROM appointments WHERE date >='{$_POST['fromdate']}' AND date <='{$_POST['todate']}' ORDER BY date");
+            $nrows += mysqli_num_rows($appt_result);
+        }
+    }
+    else if($_POST['patientsearch'])
+    {
+        $_POST['patientid'] = mysqli_real_escape_string($link, $_POST['patientid']);
+        $result = mysqli_query($link, "SELECT * FROM vac_schedule WHERE p_id ='{$_POST['patientid']}' AND given='N' ORDER BY date");
+        $nrows = mysqli_num_rows($result);
+
+        if ($include_appointments) {
+            $appt_result = mysqli_query($link, "SELECT * FROM appointments WHERE p_id ='{$_POST['patientid']}' ORDER BY date");
+            $nrows += mysqli_num_rows($appt_result);
+        }
+    }
 ?>
 
 <script type="text/javascript">
 
-	<?php for ($i=0; $i < $nrows; $i++) { ?>
+    <?php for ($i=0; $i < $nrows; $i++) { ?>
 
-			$(function() {
-				$( <?php echo "\"#vac_date".$i."\""; ?> ).datepicker({
-					changeMonth: true,
-					changeYear: true,
-					yearRange: "1970:2032",
-					dateFormat:"d M yy"
-				});
-			});
+            $(function() {
+                $( <?php echo "\"#vac_date".$i."\""; ?> ).datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    yearRange: "1970:2032",
+                    dateFormat:"d M yy"
+                });
+            });
 
-	<?php } ?>
+    <?php } ?>
 
 </script>
 
@@ -66,83 +83,127 @@ if($_POST['specificdate']||$_POST['tofromdate']||$_POST['patientsearch'])	//If s
 <input type="submit" name="sendemail" value="Send Email" style="float:right;margin-right:20px" />
 <textarea rows="3" cols="80" name="customsms"></textarea>
 <table>
-	<tbody>
-		<tr>
-			<th>Given</th>
-			<th>ID</th>
-			<th>Patient</th>
-			<th>Vaccine</th>
-			<th>Scheduled Date</th>
-			<th>Phone</th>
-			<th>Send SMS</th>
-		</tr>
+    <tbody>
+        <tr>
+            <th>Type</th>
+            <th>Given</th>
+            <th>ID</th>
+            <th>Patient</th>
+            <th>Vaccine/Appointment</th>
+            <th>Scheduled Date</th>
+            <th>Time</th>
+            <th>Phone</th>
+            <th>Send SMS</th>
+        </tr>
 <?php
-	$count = 0;
-	while($row = mysqli_fetch_assoc($result))
-	{
-		$patient = mysqli_fetch_assoc(mysqli_query($link, "SELECT name, sex, id, phone, phone2, dob, active FROM patients WHERE id={$row['p_id']}"));
-		if($patient['active']==0)
-			continue;
-		$vaccine = mysqli_fetch_assoc(mysqli_query($link, "SELECT name, upper_limit, dependent FROM vaccines WHERE id={$row['v_id']}"));
-		if($vaccine['dependent'] != 0) {
-			$query_for_dependent = "SELECT vs.given as given FROM vac_schedule vs WHERE p_id={$row['p_id']} and v_id = {$vaccine['dependent']}";
-		  $dependent_schedule = mysqli_fetch_assoc(mysqli_query($link, $query_for_dependent));
-			if($dependent_schedule['given'] == 'N') {
-				continue;
-			}
-		}
+    $count = 0;
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $patient = mysqli_fetch_assoc(mysqli_query($link, "SELECT name, sex, id, phone, phone2, dob, active FROM patients WHERE id={$row['p_id']}"));
+        if($patient['active']==0)
+            continue;
+        $vaccine = mysqli_fetch_assoc(mysqli_query($link, "SELECT name, upper_limit, dependent FROM vaccines WHERE id={$row['v_id']}"));
+        if($vaccine['dependent'] != 0) {
+            $query_for_dependent = "SELECT vs.given as given FROM vac_schedule vs WHERE p_id={$row['p_id']} and v_id = {$vaccine['dependent']}";
+            $dependent_schedule = mysqli_fetch_assoc(mysqli_query($link, $query_for_dependent));
+            if($dependent_schedule['given'] == 'N') {
+                continue;
+            }
+        }
 
 ?>
-	<tr <?php
+    <tr <?php
 
 if ($row['given']=='Y')
-		{
-			echo "id=\"focus_green\"";	//green focus if vaccine has been given
-		}
-		else if (strtotime("now") < strtotime($row['date']))
-		{
-			echo "id=\"focus_yellow\"";	//yellow focus if sched date is yet to come
-		}
-		else if (($vaccine['upper_limit'] > 36500)||(strtotime("now") < strtotime("+".$vaccine['upper_limit']." days", strtotime($patient['dob']))))	//strtotime causes error if too large value is given
-		{
-			echo "id=\"focus_orange\"";	//orange focus if sched date has gone but vac can still be given
-		}
-		else
-		{
-			echo "id=\"focus_red\"";	//red focus if vaccine cant be given now
-		}
+        {
+            echo "id=\"focus_green\"";	//green focus if vaccine has been given
+        }
+        else if (strtotime("now") < strtotime($row['date']))
+        {
+            echo "id=\"focus_yellow\"";	//yellow focus if sched date is yet to come
+        }
+        else if (($vaccine['upper_limit'] > 36500)||(strtotime("now") < strtotime("+".$vaccine['upper_limit']." days", strtotime($patient['dob']))))	//strtotime causes error if too large value is given
+        {
+            echo "id=\"focus_orange\"";	//orange focus if sched date has gone but vac can still be given
+        }
+        else
+        {
+            echo "id=\"focus_red\"";	//red focus if vaccine cant be given now
+        }
 
-	?>
+    ?>
 >
-		<td>
-			<?php echo $row['given'];?>
-		</td>
-		<td>
-			<?php echo $row['p_id'];?>
-		</td>
-		<td>
-			<a href= <?php echo "\"edit-sched.php?id={$patient['id']}\""; ?> ><?php echo $patient['name']; ?></a>
-		</td>
-		<td>
-			<?php echo $vaccine['name']; ?>
-		</td>
-		<td>
-			<input type="text" name="vac_date[]" style="width:80px" <?php echo "id=\"vac_date".$count."\""; ?> value=<?php echo "\"".date('j M Y',strtotime($row['date']))."\"";?>/>
-			<input type="hidden" name="vac_id[]" style="width:80px" value=<?php echo "\"{$row['id']}\""; ?> />
-		</td>
-		<td>
-			<?php echo $patient['phone']; ?>
-			<?php if($patient['phone2']) echo "<br />" + $patient['phone2']; ?>
-		</td>
-		<td>
-			<input type="checkbox" name="send_sms_id[]" value= <?php echo "\"{$row['id']}\""; ?> phoneCount= <?php if($patient['phone2']) echo "2"; else echo "1"; ?> patientID = <?php echo $row['p_id'];?>/>
-		</td>
-	</tr>
+        <td>Vaccine</td>
+        <td>
+            <?php echo $row['given'];?>
+        </td>
+        <td>
+            <?php echo $row['p_id'];?>
+        </td>
+        <td>
+            <a href= <?php echo "\"edit-sched.php?id={$patient['id']}\""; ?> ><?php echo $patient['name']; ?></a>
+        </td>
+        <td>
+            <?php echo $vaccine['name']; ?>
+        </td>
+        <td>
+            <input type="text" name="vac_date[]" style="width:80px" <?php echo "id=\"vac_date".$count."\""; ?> value=<?php echo "\"".date('j M Y',strtotime($row['date']))."\"";?>/>
+            <input type="hidden" name="vac_id[]" style="width:80px" value=<?php echo "\"{$row['id']}\""; ?> />
+        </td>
+        <td>N/A</td>
+        <td>
+            <?php echo $patient['phone']; ?>
+            <?php if($patient['phone2']) echo "<br />" . $patient['phone2']; ?>
+        </td>
+        <td>
+            <input type="checkbox" name="send_sms_id[]" value= <?php echo "\"{$row['id']}\""; ?> phoneCount= <?php if($patient['phone2']) echo "2"; else echo "1"; ?> patientID = <?php echo $row['p_id'];?> dataType="vaccine"/>
+        </td>
+    </tr>
 <?php
-	$count++;
-	}
+    $count++;
+    }
+
+    if ($include_appointments && isset($appt_result)) {
+        while($row = mysqli_fetch_assoc($appt_result))
+        {
+            $patient = mysqli_fetch_assoc(mysqli_query($link, "SELECT name, sex, id, phone, phone2, dob, active FROM patients WHERE id={$row['p_id']}"));
+            // We want consultation of even inactive patients, so commenting out the following line.
+			// This is because we want to send SMS to patients who have appointments, even if they are inactive for vaccines.
+			// if($patient['active']==0)
+            //     continue;
 ?>
-	</tbody>
+    <tr id="focus_blue">
+        <td>Appointment</td>
+        <td>N/A</td>
+        <td>
+            <?php echo $row['p_id'];?>
+        </td>
+        <td>
+            <a href= <?php echo "\"edit-sched.php?id={$patient['id']}\""; ?> ><?php echo $patient['name']; ?></a>
+        </td>
+        <td>
+			<?php echo $row['comment'];?>
+		</td>
+        <td>
+            <?php echo date('j M Y',strtotime($row['date']));?>
+        </td>
+        <td>
+            <?php echo $row['time'];?>
+        </td>
+        <td>
+            <?php echo $patient['phone']; ?>
+            <?php if($patient['phone2']) echo "<br />" . $patient['phone2']; ?>
+        </td>
+        <td>
+            <input type="checkbox" name="send_sms_id[]" value= <?php echo "\"{$row['id']}\""; ?> phoneCount= <?php if($patient['phone2']) echo "2"; else echo "1"; ?> patientID = <?php echo $row['p_id'];?> dataType="appointment"/>
+        </td>
+    </tr>
+<?php
+            $count++;
+        }
+    }
+?>
+    </tbody>
 </table>
 <input type="submit" name="save" value="Save Changes" />
 <input type="button" name="check" value="Check All" onClick="checkAll()" style="float:right;margin-right:20px" />
@@ -152,6 +213,7 @@ if ($row['given']=='Y')
 <input type="submit" name="sendemail" value="Send Email" style="float:right;margin-right:20px"/>
 <textarea rows="3" cols="80" name="customsms"></textarea>
 </form>
+
 <?php
 }
 else if(isset($_POST['save']))
--- search-sched.php
+++ search-sched.php
@@ -1,48 +1,39 @@
+
+
 <?php include('header.php'); ?>
 <script>
 $(function() {
-	$( "#date" ).datepicker({
-		changeMonth: true,
-		changeYear: true,
-		yearRange: "1970:2032",
-		dateFormat:"d M yy"
-	});
+    $( ".datepicker" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "1970:2032",
+        dateFormat:"d M yy"
+    });
 });
-$(function() {
-	$( "#fromdate" ).datepicker({
-		changeMonth: true,
-		changeYear: true,
-		yearRange: "1970:2032",
-		dateFormat:"d M yy"
-	});
-});
-$(function() {
-	$( "#todate" ).datepicker({
-		changeMonth: true,
-		changeYear: true,
-		yearRange: "1970:2032",
-		dateFormat:"d M yy"
-	});
-});
-
 </script>
 <h3>Search Schedule</h3>
 <a href="search-scheddg.php">Search for vaccines that have already been given</a>
 <form action="search-sched-results.php" method="post" enctype="multipart/form-data" style="width:auto" name="1">
-	<label for="date">Show schedules for : &nbsp;&nbsp;&nbsp;&nbsp;</label>
-	<input type="text" name="date" id="date" style="margin-right:40px;"/>
-	<input type="submit" name="specificdate" value="Go" />
+    <label for="date">Show schedules for : &nbsp;&nbsp;&nbsp;&nbsp;</label>
+    <input type="text" name="date" id="date" class="datepicker" style="margin-right:40px;"/>
+    <input type="checkbox" name="include_appointments" id="include_appointments" checked>
+    <label for="include_appointments">Include consultation appointments</label>
+    <input type="submit" name="specificdate" value="Go" />
 </form>
 <form action="search-sched-results.php" method="post" enctype="multipart/form-data" style="width:auto" name="2">
-	<label for="fromdate">From : &nbsp;&nbsp;&nbsp;&nbsp;</label>
-	<input type="text" name="fromdate" id="fromdate" style="margin-right:40px;"/>
-	<label for="todate">To : &nbsp;&nbsp;&nbsp;&nbsp;</label>
-	<input type="text" name="todate" id="todate" style="margin-right:40px;"/>
-	<input type="submit" name="tofromdate" value="Go" />
+    <label for="fromdate">From : &nbsp;&nbsp;&nbsp;&nbsp;</label>
+    <input type="text" name="fromdate" id="fromdate" class="datepicker" style="margin-right:40px;"/>
+    <label for="todate">To : &nbsp;&nbsp;&nbsp;&nbsp;</label>
+    <input type="text" name="todate" id="todate" class="datepicker" style="margin-right:40px;"/>
+    <input type="checkbox" name="include_appointments" id="include_appointments2" checked>
+    <label for="include_appointments2">Include consultation appointments</label>
+    <input type="submit" name="tofromdate" value="Go" />
 </form>
 <form action="search-sched-results.php" method="post" enctype="multipart/form-data" style="width:auto" name="3">
-	<label for="patientid">Patient ID : &nbsp;&nbsp;&nbsp;&nbsp;</label>
-	<input type="text" name="patientid" id="patientid" style="margin-right:40px;"/>
-	<input type="submit" name="patientsearch" value="Go" />
+    <label for="patientid">Patient ID : &nbsp;&nbsp;&nbsp;&nbsp;</label>
+    <input type="text" name="patientid" id="patientid" style="margin-right:40px;"/>
+    <input type="checkbox" name="include_appointments" id="include_appointments3" checked>
+    <label for="include_appointments3">Include consultation appointments</label>
+    <input type="submit" name="patientsearch" value="Go" />
 </form>
 <?php include('footer.php'); ?>
